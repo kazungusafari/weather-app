60af77517408438a7792c12bfae974ca91de7eec
Kazungu Safari <peter.kazungu.safari@gmail.com>
2021-10-21 17:10:46 +0300

    remove unused files and variables

--- src/hooks/useAsynThunks.ts
+++
@@ -1,8 +0,0 @@
-import { bindActionCreators } from "redux"
-import { useDispatch } from 'react-redux'
-import { HistoricalWeatherInformationInputs } from "../types";
-
-export const useAsyncThunks = (asyncThunks: { getHistoricalWeatherInfoByCityAsyncThunk: (args: HistoricalWeatherInformationInputs) => void, getWeatherInfoByCityAsyncThunk: (arg: string) => void }) => {
-    const dispatch = useDispatch()
-    return bindActionCreators(asyncThunks, dispatch)
-}
